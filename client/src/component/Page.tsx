efed7e4df972f88d659276a85309b9faa9b96400
Raphael <raphaelarzoin173@gmail.com>
2025-02-18 17:25:30 +0100

    [client] Component Page created wth Navbar and Footer

---
+++ client/src/component/Page.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from "react";
+import { Navbar } from "./Navbar"
+import { Footer } from "./Footer";
+
+export const Page = ( {children}:PropsWithChildren)=>{
+
+    return(
+       <> 
+        <Navbar></Navbar>
+        <div className="min-w-96 min-h-screen bg-indigo-50 flex justify-center  mt-3">
+          {children}
+        </div>
+        <Footer></Footer>
+        </>
+
+    )
+}
