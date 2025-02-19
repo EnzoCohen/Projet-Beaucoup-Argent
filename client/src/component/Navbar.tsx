450e1befed196ac5e339871a8282fdd4ec3f5892
enzocohen <steffetenzo@gmail.com>
2025-02-19 11:38:41 +0100

    Page de login front + Route

--- client/src/component/Navbar.tsx
+++ client/src/component/Navbar.tsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 export const Navbar = ()=> {
     return(
     
@@ -26,47 +28,29 @@ export const Navbar = ()=> {
             </div>
             <a className="btn btn-ghost text-xl">Plannify</a>
           </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li><a>Item 1</a></li>
-              <li>
-                <details>
-                  <summary>Parent</summary>
-                  <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                  </ul>
-                </details>
-              </li>
-              <li><a>Item 3</a></li>
-            </ul>
+          <div className="navbar-center hidden lg:flex flex-grow">
+            <input type="text" placeholder="Search" className="input  w-100 md:w-auto rounded-r-lg"/>
+            <input type="text" placeholder="Search" className="input  w-100 md:w-auto rounded-l-lg"/>
+
           </div>
           <div className="navbar-end">
           <div className="flex gap-2">
-            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto"/>
-            <div className="dropdown dropdown-end">
-              <div tabIndex="0" role="button" className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img
-                      alt="Tailwind CSS Navbar component"
-                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"/>
-                </div>
+              <div className="dropdown dropdown-end">
+                  <Link to={"/login"}>
+                  <button className="btn bg-white text-black border-[#e5e5e5]" >
+                      <svg aria-label="Email icon" width="16" height="16" xmlns="http://www.w3.org/2000/svg"
+                           viewBox="0 0 24 24">
+                          <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="black">
+                              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                          </g>
+                      </svg>
+                      Se connecter
+                  </button>
+                  </Link>
               </div>
-              <ul
-                  tabIndex="0"
-                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li><a>Settings</a></li>
-                <li><a>Logout</a></li>
-              </ul>
-            </div>
           </div>
-        </div>
+          </div>
         </div>
     )
 }
