83711a3d97dec12df639498cea94683445e77782
enzocohen <steffetenzo@gmail.com>
2025-04-02 13:18:28 +0200

    erreur d'enzo

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,5 +1,4 @@
 import {Stack} from "expo-router";
-import {NavigationContainer} from "@react-navigation/native";
 
 export default function StackLayout() {
     return <Stack screenOptions={{headerShown: false}}/>;
--- mobile/src/components/Home/Navbar.tsx
+++ mobile/src/components/Home/Navbar.tsx
@@ -2,7 +2,7 @@ import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useFonts } from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Comfortaa_300Light, Comfortaa_700Bold, Comfortaa_600SemiBold, Comfortaa_400Regular, Comfortaa_500Medium } from "@expo-google-fonts/comfortaa";
-import { router } from 'expo-router';
+import {router} from "expo-router";
 
 export default function Navbar() {
     let [fontsLoaded] = useFonts({
@@ -17,7 +17,7 @@ export default function Navbar() {
         <View style={{ height: 380, backgroundColor: '#18b6a4', justifyContent: 'center', alignItems: 'center', borderBottomLeftRadius: 20, borderBottomEndRadius: 20, paddingBottom: 20,  }}>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%', paddingHorizontal: 20, marginTop: 50 }}>
                 <Text style={{ fontWeight: 'bold', fontSize: 30, color: '#ffffff', fontFamily: "Comfortaa_700Bold" }}>Plannify</Text>
-                <TouchableOpacity onPress={() => { router.push('/login')}}>
+                <TouchableOpacity onPress={()=>{ router.push('/login')}}>
                     <Text style={{ fontWeight: 'bold', fontSize: 15, color: '#ffffff', fontFamily: "Comfortaa_700Bold",  }}>
                         Se connecter
                     </Text>
